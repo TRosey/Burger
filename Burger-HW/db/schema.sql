45c18ae17f2772079a016ec7f7cecd33a1aa4947
TRosey <tomhenryrosenberg@gmail.com>
2017-01-29 20:01:22 -0600

    Add files via upload

---
+++ Burger-HW/db/schema.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burgers
+(
+id int NOT NULL AUTO_INCREMENT, burger_name VARCHAR(100) NOT NULL, devoured BOOLEAN NOT NULL, date TIMESTAMP NOT NULL, PRIMARY KEY (id)
+);
